a26f2d7f709d5d4f72b47e9fcd98b92d3ef35721
amdnet <machmudseo@gmail.com>
2021-10-12 01:40:59 +0700

    update

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -11,6 +11,9 @@
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 3.2.0-rc
   </div>
+
+
+
 </footer>
 </div><!-- ./wrapper -->
 
--- app/Views/layout/navigasi.php
+++ app/Views/layout/navigasi.php
@@ -1,7 +1,7 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
     
-    <li class="nav-item">
+    <li class="nav-item ">
         <a href="<?= base_url('dashboard') ?>" class="nav-link">
         <i class="nav-icon fas fa-tachometer-alt"></i>
         <p> Dashboard </p>
